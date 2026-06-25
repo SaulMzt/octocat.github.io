ad0f840486144c4e85f9e0fe5fcd6bff553aa7ec
SaulMzt <99756469+SaulMzt@users.noreply.github.com>
2026-06-25 00:40:16 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAk7XkszEo_buPqYJccg2L-pEmzABmkNwY",
+  authDomain: "capacitacion-e88ed.firebaseapp.com",
+  projectId: "capacitacion-e88ed",
+  storageBucket: "capacitacion-e88ed.firebasestorage.app",
+  messagingSenderId: "358035727922",
+  appId: "1:358035727922:web:35816820be969543847e2b",
+  measurementId: "G-6YJP4EL21B"
 };
 
 export const ADMIN_PASSWORD_HASH = "3e12f88289504ab499f86425937a20a0a94a7c61a85dff6e2ef87bbbb0a2b9b6";
